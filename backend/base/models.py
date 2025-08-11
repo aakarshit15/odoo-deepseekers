c1a1d11bf1d9884b2effb3d6d0505182ae0166a6
aarushitahil <aarushi.tahil@gmail.com>
2025-08-11 16:32:45 +0530

    edited model

--- backend/base/models.py
+++ backend/base/models.py
@@ -87,7 +87,6 @@ class Court(models.Model):
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='courts')
     name = models.CharField(max_length=100)
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
-    price_per_hour = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return f"{self.name} - {self.venue.name}"
